2ea7bffe0cc6442d8b4db692726dd4135aef70bf
Ahmad Jamil <ahmadalktnany383@gmail.com>
2022-12-16 17:23:39 +0200

    this is the last version

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/persentation/add_edit_note/components/AddEditNoteScreen.kt
@@ -0,0 +1,16 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.persentation.add_edit_note.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.plcoding.cleanarchitecturenoteapp.feature_note.persentation.add_edit_note.AddEditNoteViewNModel
+
+@Composable
+fun AddEditNoteScreen(
+    navController: NavController,
+    noteColor:Int,
+    viewModel: AddEditNoteViewNModel= hiltViewModel(),
+) {
+
+}
